9da1e600d67376342efe627ab7b3998e146927dd
assentorp <pa@tosider.dk>
2018-10-18 16:41:34 +0200

    Adjusted waiting screen

--- src/components/Wait/index.jsx
+++ src/components/Wait/index.jsx
@@ -5,6 +5,7 @@ import { Redirect } from 'react-router'
 
 import KarnovWait from './images/karnovWait.png';
 import KarnovWaitHand from './images/karnovWaitHand.png';
+import KarnovMeteor from './images/karnovMeteor.png';
 import './wait.module.css'
 
 class Wait extends React.Component {
@@ -13,6 +14,9 @@ class Wait extends React.Component {
     return (
       <div className="wait-container">
         <h1 className="headline">Please wait</h1>
+        <img className="waitKarnovMeteor" src={KarnovMeteor} />
+        <img className="waitKarnovMeteor two" src={KarnovMeteor} />
+        <img className="waitKarnovMeteor three" src={KarnovMeteor} />
         <div className="waiting">
           <img className="waitMan" src={KarnovWait} />
           <img className="waitManHand" src={KarnovWaitHand} />
