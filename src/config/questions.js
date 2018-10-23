784d214b17cade4d488f94c2ac3b95a7464bb04b
Larsson, Nichlas Brødegaard <nichlas.larsson@karnovgroup.com>
2018-10-23 11:09:25 +0200

    More questions, more videos

--- src/config/questions.js
+++ src/config/questions.js
@@ -45,15 +45,30 @@ export default [
     answers: ['Homer going into space', 'The death of Maude Flanders', 'Mr Burns being shot', 'Homer and Ned getting married in Las Vegas'],
     type: 'text',
     correct_answer: 1,
-  }
-  ,
+  },
+  {
+    text: 'What was the cheat code to go become invulnerable in Doom 1?',
+    answers: ['IDNOCLIP', 'IDKFA', 'IDDQD', 'IDDT'],
+    type: 'text',
+    src: 'Doom1.mp4',
+    correct_answer: 2,
+  },
   {
     text: 'In what year was Google founded?',
     answers: [1995,1996, 1997, 1998],
     type: 'text',
     correct_answer: 3,
-  }
-  ,
+  },{
+    "text": "Which basketball player was in Space Jam?",
+    "answers": [
+      "Tim Hardaway",
+      "Michael Jordan",
+       "Scotty Pippen",
+      "Dennis Rodman"
+    ],
+    src: 'SpaceJam.mp4',
+    "correct_answer": 1
+  },
   {
     text: 'What day did the 31st December 1999 fall on?',
     answers: ['Thursday', 'Friday', 'Saturday', 'Sunday'],
@@ -70,6 +85,7 @@ export default [
     text: 'Which band formed first: NSYNC or the Backstreet Boys?',
     answers: ['NSYNC', 'Backstreet Boys'],
     type: 'text',
+    src: 'Boys.mp4',
     correct_answer: 1,
   },
   {
@@ -77,11 +93,101 @@ export default [
     answers: ['Clippit', 'Clippy', 'Clipster', 'Clippa'],
     type: 'text',
     correct_answer: 0,
+  },{
+     "text": "What was Britney Spears first song?",
+     "answers": [
+       "Baby one more time",
+       "Toxic",
+        "You drive me crazy",
+       "Sometimes"
+     ],
+     src: 'Toxic.mp4',
+     "correct_answer": 0
+   },
+  {
+     "text": "What was the boy’s name in Pokemon?",
+     "answers": [
+       "Kai",
+       "Ash",
+        "Onix",
+       "Poli"
+     ],
+     "correct_answer": 1
+   },
+  {
+     "text": "Tamagotchi is a Japanese mix of what two words?",
+     "answers": [
+       "Egg + Computer",
+       "Egg + Pet",
+        "Egg + Watch",
+       "Egg + Friend"
+     ],
+     "correct_answer": 2
+   },
+  {
+     "text": "Which of the following was NOT a player in Super Mario Cart?",
+     "answers": [
+       "Toad",
+       "Princess Peach",
+        "Crash",
+       "Luigi"
+     ],
+     "correct_answer": 2
+   },
+  {
+     "text": "In Spice Girls, Mel B was known as…?",
+     "answers": [
+       "Posh Spice",
+       "Scary Spice",
+        "Sporty Spice",
+       "Sweet Spice"
+     ],
+     "correct_answer": 1
+   },
+  {
+    "text": "Which of the following is NOT an actual disease from the game Theme Hospital?",
+    "answers": [
+      "Irrigated bowels",
+      "3rd Degree Sideburns",
+      "Corrugated Ankles",
+      "TV Personalities"
+    ],
+    "correct_answer": 0
   },
   {
-    text: 'What was the cheat code to go become invulnerable in Doom 1?',
-    answers: ['IDNOCLIP', 'IDKFA', 'IDDQD', 'IDDT'],
-    type: 'text',
-    correct_answer: 2,
+    "text": "When was Napster created?",
+    "answers": [
+      "1997",
+      "1998",
+      "1999",
+      "2000"
+    ],
+    "correct_answer": 2
+  },
+  {
+    "text": "Which was the highest grossing movie of the '90s?",
+    "answers": [
+      "Titanic",
+      "Jurassic park"
+    ],
+    "correct_answer": 0
+  },
+  {
+    "text": "Which '90s movie was released first?",
+    "answers": [
+      "Terminator 2",
+      "Robocop 2"
+    ],
+    "correct_answer": 1
+  },
+  {
+    "text": "Which Michael Jackson song was released first?",
+    "answers": [
+      "Blood on the Dance Floor",
+      "Earth Song",
+      "Ghost",
+      "Remember the Time"
+    ],
+    "correct_answer": 3
   }
 ]
